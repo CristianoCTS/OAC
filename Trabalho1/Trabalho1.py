0fd4a26c8c3e1529b4ce3aee8f4a05e9514a1e54
Cristiano <cristolsan.cts.droid@droidunb.com>
2024-10-30 15:24:58 -0300

    etruturação

--- Trabalho1/Trabalho1.py
+++ Trabalho1/Trabalho1.py
@@ -4,3 +4,19 @@ import numoy as np
 #base
 mem = np.zeros(16384, dtype = np.uint8)
 
+#functions
+
+def lb(reg, kte):
+    pass
+
+def lbu(reg, kte):
+    pass
+
+def lw(reg, kte):
+    pass
+
+def sb(reg, kte, byte):
+    pass
+
+def sw(reg, kte, word):
+    pass
