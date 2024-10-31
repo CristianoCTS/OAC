756f10c9fc691129a829fdee0ddff1ad3437b560
Cristiano <cristolsan.cts.droid@droidunb.com>
2024-10-31 10:20:54 -0300

    Estruturação

--- Trabalho1/Trabalho1.py
+++ Trabalho1/Trabalho1.py
@@ -7,16 +7,41 @@ mem = np.zeros(16384, dtype = np.uint8)
 #functions
 
 def lb(reg, kte):
+    address = reg + kte
+    byte = np.int8(mem[address])
+    if byte < 0:
+        word = (1 << 32) + byte
     pass
 
 def lbu(reg, kte):
+    address = reg + kte
+    byte = np.int8(mem[address])
+    if byte < 0:
+        word = (1 << 32) + byte
     pass
 
 def lw(reg, kte):
+    address = reg + kte
+    byte = np.int8(mem[address])
+    if byte < 0:
+        word = (1 << 32) + byte
     pass
 
 def sb(reg, kte, byte):
+    address = reg + kte
+    byte = np.int8(mem[address])
+    if byte < 0:
+        word = (1 << 32) + byte
     pass
 
 def sw(reg, kte, word):
+    address = reg + kte
+    byte = np.int8(mem[address])
+    if byte < 0:
+        word = (1 << 32) + byte
     pass
+
+sb(7, 0, 0xaa)
+word = lb(7, 0)
+print("word_10 = ", word)
+print("word_x = ", hex(word))
