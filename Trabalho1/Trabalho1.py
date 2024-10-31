45b661fefe72c824422a4079acb1f4d012418880
Cristiano <cristolsan.cts.droid@droidunb.com>
2024-10-31 12:08:43 -0300

    Quem diria que ler os slides da matérria ajudaria a entender a matéria huh

--- Trabalho1/Trabalho1.py
+++ Trabalho1/Trabalho1.py
@@ -1,5 +1,5 @@
 #imports
-import numoy as np
+import numpy as np
 
 #base
 mem = np.zeros(16384, dtype = np.uint8)
@@ -9,39 +9,43 @@ mem = np.zeros(16384, dtype = np.uint8)
 def lb(reg, kte):
     address = reg + kte
     byte = np.int8(mem[address])
-    if byte < 0:
-        word = (1 << 32) + byte
-    pass
+    return np.uint32(byte)
 
 def lbu(reg, kte):
     address = reg + kte
-    byte = np.int8(mem[address])
-    if byte < 0:
-        word = (1 << 32) + byte
-    pass
+    byte = np.uint8(mem[address])
+    return np.uint32(byte)
 
 def lw(reg, kte):
     address = reg + kte
-    byte = np.int8(mem[address])
-    if byte < 0:
-        word = (1 << 32) + byte
-    pass
+    word = (mem[address + 3] << 24) | (mem[address + 2] << 16) | (mem[address + 1] << 8) | mem[address]
+    return np.uint32(word)
 
 def sb(reg, kte, byte):
     address = reg + kte
-    byte = np.int8(mem[address])
-    if byte < 0:
-        word = (1 << 32) + byte
-    pass
+    mem[address] = np.uint8(byte)
 
 def sw(reg, kte, word):
     address = reg + kte
-    byte = np.int8(mem[address])
-    if byte < 0:
-        word = (1 << 32) + byte
-    pass
+    mem[address] = np.uint8(word & 0xFF)
+    mem[address + 1] = np.uint8((word >> 8) & 0xFF)
+    mem[address + 2] = np.uint8((word >> 16) & 0xFF)
+    mem[address + 3] = np.uint8((word >> 24) & 0xFF)
 
-sb(7, 0, 0xaa)
-word = lb(7, 0)
-print("word_10 = ", word)
-print("word_x = ", hex(word))
+#testing
+
+sw(0,	0,	0xABACADEF)
+sb(4,	0,	1)
+sb(4,	1,	2)
+sb(4,	2,	3)
+sb(4,	3,	4)
+
+print(hex(lw(0,0)))	
+print(hex(lb(0,0)))
+print(hex(lb(0,1)))
+print(hex(lb(0,2)))
+print(hex(lb(0,3)))
+print(hex(lbu(0,0)))
+print(hex(lbu(0,1)))
+print(hex(lbu(0,2)))
+print(hex(lbu(0,3)))
